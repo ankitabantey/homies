aa4033fac4f8314f8656075a7caf366fdd1862e4
Ankita Bantey <38222723+ankitabantey@users.noreply.github.com>
2020-12-05 22:11:17 -0600

    the representatives page

---
+++ _site/src/Pages/page1.js
@@ -0,0 +1,16 @@
+import './page1.css';
+import React from "react";
+
+
+
+function Page1() {
+
+    return (
+        <div className = "page1">
+            <h1>FindMyHomie</h1>
+        </div>
+        
+    );
+}
+
+export default Page1;
---
+++ _site/src/Pages/page2.js
@@ -0,0 +1,17 @@
+import './page2.css';
+import React from "react";
+/* Chat function on this page */
+
+
+function chat() {
+
+    return (
+        <div className = "chat">
+            <h1>FindMyHomie Page 2</h1>
+            <h2> Chat function can go here</h2>
+        </div>
+        
+    );
+}
+
+export default chat;
